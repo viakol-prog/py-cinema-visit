e3ad6cae82f24b73630a4879802f56e65d7249f9
Viacheslav Kolesnyk <viakol.lokaiv@gmail.com>
2026-03-28 21:47:06 +0200

    Fix Implement cinema visit: CinemaBar, CinemaHall, Customer, Cleaner and main.cinema_visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.hall import CinemaHall
+from app.cinema.bar import CinemaBar
+from typing import List, Dict
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    movie: str,
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str
+) -> None:
+    customers_list = []
+
+    for person in customers:
+        customer = Customer(name=person["name"], food=person["food"])
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+        customers_list.append(customer)
+
+    cleaner_instance = Cleaner(name=cleaner)
+    hall = CinemaHall(number=hall_number)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customers_list,
+        cleaning_staff=cleaner_instance
+    )
